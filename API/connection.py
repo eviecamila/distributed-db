fff2d87dc73e71d744dc7aac9191a618b98f99c4
MinMinAburrida <pcvvblah@gmail.com>
2024-05-07 14:29:30 -0700

    API Hecha
    Falta cambiar datos para hacerlo jalar con CENTOS

--- API/connection.py
+++ API/connection.py
@@ -1,6 +1,19 @@
 import mysql.connector
+# import csv
 
-# Configuración de la conexión
+
+# TODO: CAMBIAR DATOS DE ACCESO A LA DB en CENTOS
+
+"""
+# evie mamadisima
+# Script para instalar la API cagada
+yum install git
+git clone https://github.com/eviecamila/distributed-db.git
+cd API
+pip install -r requirements.txt
+py main.py
+"""
+
 config = {
   'user': 'evie',
   'password': 'evie',
@@ -9,26 +22,32 @@ config = {
   'raise_on_warnings': True
 }
 
-def connect():
-        # Establecer la conexión
+def select_query(query="SELECT * FROM cdr"):
     try:
         conn = mysql.connector.connect(**config)
         print("Conexión exitosa a la base de datos")
+        print(f"Ejecutando '{query}'")
         
-        # Ejecutar una consulta
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM cdr")
+        cursor.execute(query)
+        data = cursor.fetchall()
+        # Crear un archivo CSV y escribir los resultados
+        # with open('resultados_cdr.csv', 'w', newline='') as csvfile:
+            # csvwriter = csv.writer(csvfile)
+            
+            # Escribir el encabezado del CSV
+            # csvwriter.writerow([i[0] for i in cursor.description])
+            
+            # Escribir los datos de la consulta al CSV
+            # csvwriter.writerows(cursor)
         
-        # Obtener los resultados
-        for row in cursor.fetchall():
-            print(row)
+        # print("Los resultados se han guardado en 'resultados_cdr.csv'")
         
-        # Cerrar el cursor y la conexión
         cursor.close()
         conn.close()
+        return data
 
     except mysql.connector.Error as err:
         print("Error de conexión a la base de datos:", err)
 
-
-connect()
+# connect_and_write_to_csv()
--- API/main.py
+++ API/main.py
@@ -1,14 +1,19 @@
-from flask import Flask, jsonify
 
+from flask import Flask, jsonify, request
+import connection
 # Creamos una instancia de Flask
 app = Flask(__name__)
 
 # Definimos una ruta para el endpoint principal
+
+
 @app.route('/')
 def index():
     return "¡Hola! Esta es mi API."
 
 # Definimos una ruta para un endpoint que devuelve datos JSON
+
+
 @app.route('/api/ejemplo', methods=['GET'])
 def ejemplo():
     datos = {
@@ -16,6 +21,80 @@ def ejemplo():
         'status': 'success'
     }
     return jsonify(datos)
+
+
+@app.route('/calls', methods=['GET'])
+def llamadas():
+    try:
+        data = {'src':None, 'dst':None, 'status':None}
+        try:data['src'] = request.args['s']
+        except: pass
+        try:data['dst'] = request.args['d']
+        except: pass
+        try:data['status'] = request.args['status']
+        except: pass
+        
+        # Construir la consulta SQL
+        query = f"SELECT * FROM cdr"
+        # Construir la consulta SQL
+        query = "SELECT * FROM cdr"
+        put_where = False
+        if data['src'] or data['dst'] or data['status']:
+            query += " WHERE "
+            if data['src']:
+                query += f"src = '{data['src']}'"
+                put_where = True
+            if data['dst']:
+                if put_where:
+                    query += " AND "
+                query += f"dst = '{data['dst']}'"
+                put_where = True
+            if data['status']:
+                if put_where:
+                    query += " AND "
+                query += f"status = '{data['status']}'"
+                put_where = True
+        # Completar query si existen los datos:
+
+        # Obtener los resultados
+        results = connection.select_query(query)
+        
+        # Convertir los resultados a un formato JSON
+        json_data = []
+        for row in results:
+            json_data.append({
+                'calldate': row[0],
+                'clid': row[1],
+                'src': row[2],
+                'dst': row[3],
+                'dcontext': row[4],
+                'channel': row[5],
+                'dstchannel': row[6],
+                'lastapp': row[7],
+                'lastdata': row[8],
+                'duration': row[9],
+                'billsec': row[10],
+                'disposition': row[11],
+                'amaflags': row[12],
+                'accountcode': row[13],
+                'uniqueid': row[14],
+                'userfield': row[15],
+                'did': row[16],
+                'recordingfile': row[17],
+                'cnum': row[18],
+                'cnam': row[19],
+                'outbound_cnum': row[20],
+                'outbound_cnam': row[21],
+                'dst_cnam': row[22],
+                'linkedid': row[23],
+                'peeraccount': row[24],
+                'sequence': row[25]
+            })
+
+        return jsonify(json_data)
+
+    except Exception as e:
+        print("Error:", e)
 
 if __name__ == '__main__':
     # Ejecutamos la aplicación en el puerto 5000
