46e2a376d012a927da2b99cef19f3807939d3f10
MinMinAburrida <pcvvblah@gmail.com>
2024-05-08 18:00:50 -0700

    adios

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -28,9 +28,7 @@ export class AppComponent {
   filtro: string = '';
   ciudad: string = '';
   estadoLlamada: string = '';
-
-
-  estados: any = [
+  estados = [
     { value: 'A', status: 'Contestada' },
     { value: 'B', status: 'Ocupado' },
     { value: 'N', status: 'Sin Respuesta' },
@@ -41,9 +39,7 @@ export class AppComponent {
     { value: 'N', name: 'Navojoa' },
     { value: 'O', name: 'Obregon' }
   ];
-  estadosLlamada: any = {
-    ANSWERED: 'Contestada', BUSY: 'Ocupado', 'NO ANSWER': 'Sin Respuesta', 'FAILED': 'Fallida'
-  }
+
   constructor(private api: ApiService, private PDF: ReportService) { }
 
   ngOnInit(): void {
@@ -55,48 +51,19 @@ export class AppComponent {
     this.api.getCalls({
       d: '', c: this.ciudad, e: this.estadoLlamada, d1: this.fechaInicio, d2: this.fechaFin
     }).subscribe((data: any) => {
-      // Formatea las fechas antes de asignar los datos a this.calls
       this.results = true;
-      this.calls = data.map((call: any) => {
-        // Formatea las fechas utilizando la función de formateo
-        call.calldate = this.formatDate(call.calldate);
-        return call;
-      });
+      this.calls = data;
       console.log(data)
     });
   }
-  displayedColumns: string[] = [
-
-    'src',
-    'dst',
-    'duration',
-    'disposition',
-    'calldate',
-    "ciudad"
-  ];
-  formatDate(dateString: string): string {
-    const fechaOriginal = new Date(dateString);
-    const opcionesDeFormato: Intl.DateTimeFormatOptions = {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false // Para usar el formato de 24 horas
-    };
-    return fechaOriginal.toLocaleString('en-US', opcionesDeFormato);
-  }
-
-
-
   generarReporte() {
     // Llama al método buscar() de ApiService
-    // console.log(this.fechaFin, this.fechaInicio)
-    // this.api.getCalls({
-    //   d: '', c: this.ciudad, e: this.estadoLlamada, d1: this.fechaInicio, d2: this.fechaFin
-    // }).subscribe((data: any) => {
-    this.PDF.exportAsPDF(); // Pasar los datos de la tabla a exportAsPDF()
-    // });?
+    console.log(this.fechaFin, this.fechaInicio)
+    this.api.getCalls({
+      d: '', c: this.ciudad, e: this.estadoLlamada, d1: this.fechaInicio, d2: this.fechaFin
+    }).subscribe((data: any) => {
+      this.PDF.exportAsPDF(); // Pasar los datos de la tabla a exportAsPDF()
+    });
   }
 
 }
